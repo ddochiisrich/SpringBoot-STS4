416f5e3aae407885c1104626dd688b2f7aaa04b3
ddochiisrich <ddochiisrich@gmail.com>
2024-06-27 12:45:52 +0900

    오전수업

--- springbootclass-app02/src/main/java/com/springbootstudy/app/controller/MemoEntityController.java
+++ springbootclass-app02/src/main/java/com/springbootstudy/app/controller/MemoEntityController.java
@@ -1,10 +1,18 @@
 package com.springbootstudy.app.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootstudy.app.dto.MemoDTO;
@@ -19,6 +27,32 @@ import lombok.extern.slf4j.Slf4j;
 public class MemoEntityController {
 
 	private final MemoEntityService entityService;
+	
+	@PostMapping("/jpaMemos")
+	public ResponseEntity<MemoDTO> addMemo(@RequestBody MemoDTO memo) {
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(entityService.addMemo(memo));
+	}
+	
+	@PutMapping("/jpaMemos")
+	public ResponseEntity<MemoDTO> updateMemo(MemoDTO memo){
+		
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(entityService.updateMemo(memo));
+	}
+	
+	@DeleteMapping("/jpaMemos")
+	public Map<String, Object> deleteMemo(@RequestParam("no") int no){
+		entityService.deleteMemo(no);
+		
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("result", true);
+		
+		return resultMap;
+	}
+	
 	
 	@GetMapping("/jpaMemos")
 	public ResponseEntity<List<MemoDTO>> memoList(){
